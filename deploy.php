321a3e3f303567127e72f4cc20471c71d472518a
Paul <paul.souille85@gmail.com>
2019-05-23 08:43:53 +0200

    update deploy

--- deploy.php
+++ deploy.php
@@ -1,18 +1,10 @@
 <?php
-
-	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git reset --hard HEAD',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
-	$output = '';
-	foreach($commands AS $command){
-		$tmp = shell_exec($command);
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
+   $commands = array(
+           'git reset –hard HEAD',
+           'git pull',
+           'git submodule sync',
+           'git submodule update',
+   );
+   foreach($commands as $command){
+           $tmp = shell_exec($command);
+   }
